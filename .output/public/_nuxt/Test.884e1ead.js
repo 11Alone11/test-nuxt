2045c71622bf3128a59ccaab090f76e1e96536a4
11Alone11 <anton.stasyuk.0@mail.ru>
2023-11-13 01:01:14 +0300

    server

--- .output/public/_nuxt/Test.884e1ead.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o as s}from"./entry.b788fa70.js";const c={},o={class:"test"};function n(r,_){return s(),t("div",o,"test")}const f=e(c,[["render",n]]);export{f as default};
--- .output/server/chunks/app/_nuxt/_id_-styles.c39422fb.mjs
+++
@@ -1,6 +0,0 @@
-const _id__vue_vue_type_style_index_0_lang = "";
-
-const _id_Styles_c39422fb = [_id__vue_vue_type_style_index_0_lang, _id__vue_vue_type_style_index_0_lang];
-
-export { _id_Styles_c39422fb as default };
-//# sourceMappingURL=_id_-styles.c39422fb.mjs.map
